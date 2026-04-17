3ba5aeaf5fe0fbc74495b064748009e442038e75
Tim Walker <tdskywalker@gmail.com>
2026-04-17 12:06:15 -0400

    libhb: hbavfilter: XXX: log all parsed AVFilterGraphs.

--- libhb/hbavfilter.c
+++ libhb/hbavfilter.c
@@ -108,6 +108,8 @@ hb_avfilter_graph_init(hb_value_t * settings, hb_filter_init_t * init)
         goto fail;
     }
 
+    hb_log("avfilter_graph_parse2(%s)", settings_str);
+
     // Build filter input
 #if HB_PROJECT_FEATURE_QSV
     if (hb_qsv_hw_filters_are_enabled(graph->job))
