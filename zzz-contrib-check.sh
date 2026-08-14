8363a12533ffc2faf4a0f886d2edc356db8f35ca
Tim Walker <tdskywalker@gmail.com>
2026-08-14 23:01:58 +0200

    zzz-contrib-check.sh, zzz-contrib-update.sh

---
+++ zzz-contrib-check.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+git diff --stat origin/master -- \
+	contrib/amf \
+	contrib/bzip2 \
+	contrib/fdk-aac \
+	contrib/freetype \
+	contrib/fribidi \
+	contrib/jansson \
+	contrib/lame \
+	contrib/libass \
+	contrib/libdav1d \
+	contrib/libdvdnav \
+	contrib/libdvdread \
+	contrib/libgnurx \
+	contrib/libiconv \
+	contrib/libjpeg-turbo \
+	contrib/libogg \
+	contrib/libopus \
+	contrib/libspeex \
+	contrib/libtheora \
+	contrib/libvorbis \
+	contrib/libvpl \
+	contrib/libvpx \
+	contrib/nvenc \
+	contrib/svt-av1 \
+	contrib/xz \
+	contrib/zimg \
+	contrib/zlib
